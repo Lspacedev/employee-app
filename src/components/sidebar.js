e4d559873fe6b8018d5b5ab3363ed47db45ce562
Lspacedev <145987706+Lspacedev@users.noreply.github.com>
2024-07-18 12:06:43 +0200

    updated styles

--- src/components/sidebar.js
+++ src/components/sidebar.js
@@ -1,8 +1,16 @@
+import { IoPersonOutline } from "react-icons/io5";
+import { RiDashboardLine } from "react-icons/ri";
+
 function Sidebar() {
   return (
     <div className="Sidebar">
-      <div>Dashboard</div>
-      <div>Employees</div>
+      <div>
+        <RiDashboardLine />
+        Dashboard
+      </div>
+      <div>
+        <IoPersonOutline /> Employees
+      </div>
     </div>
   );
 }
